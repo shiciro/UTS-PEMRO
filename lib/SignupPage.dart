b423d2a394f88ac07ba464895481eecb56de1edf
shiciro <49854415+shiciro@users.noreply.github.com>
2021-11-07 02:11:39 +0700

    c

--- lib/SignupPage.dart
+++ lib/SignupPage.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
 
+import 'components/rounded_input_field.dart';
+import 'components/rounded_password_field.dart';
+import 'constants.dart';
+
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            height: 130,
+            width: double.infinity,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              "Sign Up",
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Text(
+            "Create new account",
+            style: TextStyle(fontSize: 25, color: Colors.grey),
+          ),
+          SizedBox(height: 25),
+          RoundedInputField(
+            hintText: "Your Email",
+            onChanged: (value) {},
+          ),
+          RoundedInputField(
+            hintText: "Confirm Email",
+            onChanged: (value) {},
+          ),
+          RoundedPasswordField(
+            onChanged: (value) {},
+          ),
+          RoundedPasswordField(
+            onChanged: (value) {},
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 90),
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/");
+                },
+                child: Text("Sign In"),
+                style: ElevatedButton.styleFrom(
+                    primary: consPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(29))))),
+          ),
+        ],
+      ),
+    );
   }
 }
